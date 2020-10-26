cd408dbcb564adbe7a509563baa095651a3a461d
Vassiliy Taranov <taranov.vv@gmail.com>
2020-10-26 10:08:12 +0300

    Update web_grep.py
    Add timeout, concurrency limit and errors handling

--- web_grep.py
+++ web_grep.py
@@ -1,22 +1,32 @@
 import sys
 import asyncio
 from argparse import ArgumentParser
-from aiohttp import ClientSession
+
+from aiohttp import ClientSession, ClientError, ClientTimeout
 
 
 parser = ArgumentParser()
 parser.add_argument('--word', help='word to find')
+parser.add_argument('--limit', default=16, help='concurrency limit')
+parser.add_argument('--timeout', default=60, help='timeout in seconds')
 args = parser.parse_args()
+
+semaphore = asyncio.Semaphore(args.limit)
+timeout = ClientTimeout(total=args.timeout)
 
 
 async def process_url(url, session, word='Python'):
-    async with session.get(url) as response:
-        if response.status != 200:
-            print(f"Can't fetch {url}")
-            return
-        text = await response.text()
-        count = text.count(word)
-        print(f"Count for {url}: {count}")
+    try:
+        async with session.get(url, timeout=timeout) as response:
+            async with semaphore:
+                if response.status != 200:
+                    print(f"Can't fetch {url}")
+                    return
+                text = await response.text()
+                count = text.count(word)
+                print(f"Count for {url}: {count}")
+    except ClientError as e:
+        print(f"Can't fetch {url}: {e}")
 
 
 async def run():
